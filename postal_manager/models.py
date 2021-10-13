86d2ce36604e0dcc4b33ce9c08deb035a3be1bd2
Jeilson Araujo <jeilsonaraujo@icloud.com>
2021-10-12 21:50:53 -0300

    Este é o primeiro commit mas o projeto ja estava em andamento a cerca de dois dias

---
+++ postal_manager/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+
+# Create your models here.
+
+class Tipos_postais_base(models.Model):
+    
+    id = models.BigAutoField(primary_key=True)
+    nome_tipo = models.CharField(max_length=30)
+    prazo = models.IntegerField()
+
+    def __str__(self):
+        return "Tipo Postal: {}, Prazo: {}".format(self.nome_tipo, self.prazo)
+
+
+class Tipos_postais_db(models.Model):
+
+    id = models.BigAutoField(primary_key=True)
+    classe_obj = models.ForeignKey(Tipos_postais_base, on_delete=models.PROTECT)
+    sigla_obj = models.CharField(max_length=2)
+
+    def __str__(self):
+        return "Tipo Postal: {}, Sigla: {}".format(self.classe_obj.nome_tipo, self.sigla_obj)
+
+
+class Objetos_db(models.Model):
+    
+    id = models.BigAutoField(primary_key=True)
+    destinatario = models.CharField(max_length=70, verbose_name="Destinatário")
+    data_inclusao = models.DateField(auto_now_add=True)
+    classe_obj = models.ForeignKey(Tipos_postais_base, on_delete=models.PROTECT, verbose_name="Tipo")
+
+    def __str__(self):
+        return "Número do Objeto: {}, Destinatário: {}, Data de Inclusão: {} - {}".format(self.id, self.destinatario, self.data_inclusao, self.classe_obj.nome_tipo)
---
+++ postal_manager/views.py
@@ -0,0 +1,36 @@
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from .models import *
+
+
+
+class Index(TemplateView):
+    template_name = 'postal_manager/index.html'
+
+class Entrega_objetos(ListView):
+    model = Objetos_db
+    template_name = 'postal_manager/entrega_objetos.html'
+
+class Cad_obj_simples(CreateView):
+    model = Objetos_db
+    fields = ['destinatario','classe_obj']
+    template_name = 'postal_manager/cadastrar_obj_simples.html'
+    success_url = reverse_lazy('cad_obj_simp')
+
+class Cad_obj_qual(TemplateView):
+    template_name = 'postal_manager/cadastrar_obj_qual.html'
+
+class Tipos_postais(CreateView):
+    model = Tipos_postais_db
+    fields = ['classe_obj','sigla_obj']
+    template_name = 'postal_manager/tipos_postais.html'
+    success_url = reverse_lazy('cad_obj_simp')
+
+
+class Obj_vencidos(TemplateView):
+
+    template_name = 'postal_manager/obj_vencidos.html'
+
+
